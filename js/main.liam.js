5e55585f1889ce80a50f28fc552d8f705a6c1f58
Liam Bolling <liam.bolling@gmail.com>
2022-02-20 17:13:43 -0500

    changes to perosnal websiote

--- js/main.liam.js
+++
@@ -1,9 +0,0 @@
-
-
-$( ".tab-button" ).click(function() {
-    $(".tab-button").removeClass("selected");
-    $(this).addClass("selected");
-    var contentName = ".content-"+$(this).attr("type");
-    $(".content").velocity("transition.slideDownOut");
-    $(contentName).delay(250).velocity("transition.slideUpIn");
-});
